04e216c957ffceedd092694ecafd0df024b03579
GG <zjjhgzh@gmail.com>
2023-02-06 10:58:06 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/promise87_notify_me/",
   plugins: [vue()],
   resolve: {
     alias: {
